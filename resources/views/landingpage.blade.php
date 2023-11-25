63793f2c9d54ed074a083d34497c3324ce0978f9
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2023-11-25 20:09:55 +0700

    buat file

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.landing')
+@extends('layouts.base')
 @section('main_content')
     <!--
       Heads up! 👋
@@ -7,7 +7,7 @@
     -->
 
     <section
-        class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+        class="relative bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('/img/banner4u.png') }})">
         <div
             class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l">
         </div>
