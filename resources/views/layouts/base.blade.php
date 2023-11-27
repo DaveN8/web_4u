ef0ef60c77fec11c2cf016cb81cbeed85a423b33
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-11-27 22:15:29 +0700

    update landing #2

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,6 +8,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
     <title>4U Invitation</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,700" />
