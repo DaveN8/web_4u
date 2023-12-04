b9fc5c15d31a7e1d877af84285423433f7e795bb
YuppyItuEnak <fmuhammad05@Student.ciputra.ac.id>
2023-12-05 01:19:43 +0700

    aselole

--- resources/views/pembelianPage.blade.php
+++ resources/views/pembelianPage.blade.php
@@ -60,16 +60,16 @@
                     </thead>
                     <tbody>
                         @foreach ($package as $pack)
-                        <tr
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="w-4 p-4">
-                                <div class="flex items-center">
-                                    <input id="checkbox-table-search-1" type="checkbox"
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                           
+                            <tr
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td class="w-4 p-4">
+                                    <div class="flex items-center">
+                                        <input id="checkbox-table-search-1" type="checkbox"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                    </div>
+                                </td>
+
                                 <th scope="row"
                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {{ $pack['fullname'] }}
@@ -90,7 +90,8 @@
                                     {{ $pack['deskripsiAcara'] }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <img class="rounded-t-lg" src="{{ asset('storage/' . $pack['buktiTransfer']) }}" alt="" />
+                                    <img class="rounded-t-lg" src="{{ asset('storage/' . $pack['buktiTransfer']) }}"
+                                        alt="" />
                                 </td>
                                 {{-- <td class="px-6 py-4">
                                 $2999
@@ -100,11 +101,9 @@
                             </td> --}}
                                 <td class="flex items-center px-6 py-4">
                                     <a href="#"
-                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    <a href="#"
                                         class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</a>
                                 </td>
-                        </tr>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
