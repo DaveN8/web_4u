8d0ecde78d22b40cc4c7b1a2da48754e48c49d9b
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-12-11 11:31:25 +0700

    update admin dashboard

--- resources/views/adminDashboard.blade.php
+++ resources/views/adminDashboard.blade.php
@@ -18,55 +18,7 @@
         // style="background: linear-gradient(to bottom, rgb({{ $color2 }}), rgb({{ $color1 }}));"
 
     @endphp
-    <main class="container mx-auto mt-8">
-        <h1 class="text-3xl font-semibold mb-4">Admin Dashboard</h1>
-
-        <!-- Contoh Tabel Data -->
-        <div class="bg-white p-4 shadow-md rounded-md mb-6">
-            <h2 class="text-xl font-semibold mb-4">Data Pengguna</h2>
-
-            <table class="min-w-full border border-gray-300">
-                <thead>
-                    <tr>
-                        <th class="py-2 px-4 border-b">ID</th>
-                        <th class="py-2 px-4 border-b">Nama</th>
-                        <th class="py-2 px-4 border-b">Email</th>
-                        <!-- Tambahkan kolom lainnya sesuai kebutuhan -->
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="py-2 px-4 border-b">1</td>
-                        <td class="py-2 px-4 border-b">John Doe</td>
-                        <td class="py-2 px-4 border-b">john@example.com</td>
-                        <!-- Tambahkan data lainnya sesuai kebutuhan -->
-                    </tr>
-                    <!-- Tambahkan baris data lainnya sesuai kebutuhan -->
-                </tbody>
-            </table>
-        </div>
-
-        <!-- Contoh Form Tambah Data -->
-        <div class="bg-white p-4 shadow-md rounded-md mb-6">
-            <h2 class="text-xl font-semibold mb-4">Tambah Pengguna Baru</h2>
-
-            <form action="{{ route('admin.users.store') }}" method="post">
-                @csrf
-                <label for="nama" class="block mb-2">Nama:</label>
-                <input type="text" id="nama" name="nama" class="w-full border px-4 py-2 mb-2">
-
-                <label for="email" class="block mb-2">Email:</label>
-                <input type="email" id="email" name="email" class="w-full border px-4 py-2 mb-2">
-
-                <!-- Tambahkan input lainnya sesuai kebutuhan -->
-
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2">Simpan</button>
-            </form>
-        </div>
-
-        <!-- Tambahkan konten admin lainnya sesuai kebutuhan -->
-
+    <main>
+        
     </main>
-
-    
 @endsection
