dda0f354f58af9993bb330f87279d3f3c08056ba
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-12-11 11:33:50 +0700

    add update dashboard + add script to main

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/gsap@3.9.0/dist/gsap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.x.x/dist/alpine.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
     <title>4U Invitation</title>
 
