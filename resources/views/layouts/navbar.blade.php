1d918729fd1d82049e54c17e13e7ad5208958b8a
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2024-01-09 06:49:58 +0700

    i

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -158,7 +158,7 @@
             <a href="{{ url('/') }}"
                 class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
 
-            <a href="{{ route('desain.index') }}"
+            <a href="{{ url('/gallery') }}"
                 class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Gallery</a>
 
         </div>
